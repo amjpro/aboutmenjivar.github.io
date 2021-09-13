85ab048f6d618161f8ed16a1ce192286de26f31e
amjpro <61218919+aboutmenjivar@users.noreply.github.com>
2021-09-13 16:44:02 -0400

    Update mobile.js

--- zook/js/mobile.js
+++ zook/js/mobile.js
@@ -1,5 +1,4 @@
 function mobile (){
-    $('.navbar').css("background-color","#363636");
     if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i)  
         || navigator.userAgent.match(/iPad/i)  
         || navigator.userAgent.match(/iPod/i) 
