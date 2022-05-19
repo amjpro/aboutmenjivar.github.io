835094d5c8c692ca1d3622ed2f278d39839bbf4a
Alex Harvey <alex11@live.unc.edu>
2022-05-19 17:06:22 -0400

    Major updates
    includes addition of resume page

--- harvey/mazegen.js
+++ harvey/mazegen.js
@@ -69,8 +69,97 @@ class Mazegen {
     blank_env(length, width) {
         let env = [];
         for (let i=0; i<length; i++) {
-            env.append([' '] * width);
+            env[env.length] = ([' '] * width);
         }
+    }
+
+    out_of_bounds(env, curr_coor) {
+        return (curr_coor[0]<0 || curr_coor[1]<0 || curr_coor[0]>env.length-1 || curr_coor[1]>env[0].length-1);
+    }
+
+    cartesian_to_env (env, xy_pair) {
+        if (out_of_bounds([env.length-xy_pair[1],xy_pair[0]])) {
+            print("ERROR: coordinate out of range of environment");
+            return [];
+        }
+        return [env.length-xy_pair[1],xy_pair[0]];
+    }
+    
+    eq (c_one, c_two) {
+        return (c_one[0] == c_two[0] && c_one[1] == c_two[1]);
+    }
+
+    dist (c_one, c_two) {
+        return (Math.abs(c_one[0] - c_two[0]) + Math.abs(c_one[1] - c_two[1]));
+    }
+
+    n (curr_coor) {
+        return [curr_coor[0]-1, curr_coor[1]];
+    }
+    s (curr_coor) {
+        return [curr_coor[0]+1, curr_coor[1]];
+    }
+    e (curr_coor) {
+        return [curr_coor[0], curr_coor[1]+1];
+    }
+    w (curr_coor) {
+        return [curr_coor[0], curr_coor[1]-1];
+    }
+    nsew (curr_coor) {
+        return [[curr_coor[0]-1, curr_coor[1]],
+                [curr_coor[0]+1, curr_coor[1]],
+                [curr_coor[0], curr_coor[1]+1],
+                [curr_coor[0], curr_coor[1]-1]];
+    }
+
+    list_blank_directions(env, curr_coor) {
+        let ret = [];
+        let nsew_coors = nsew(curr_coor);
+        for (let i = 0; i < 4; i++) {
+            if (!out_of_bounds(env, nsew_coors[i])) {
+                if (env[nsew_coors[i][0]][nsew_coors[i][1]] == " ") {
+                    ret[ret.length] = nsew_coors[i];
+                }
+            }
+        }
+        return ret;
+    }
+
+    list_accessible_coordinates(env, curr_coor) {
+        // A flood fill function like iterscan that returns all coordinates in flood of area
+        let active_coors = [curr_coor];
+        let inactive_coors = [];
+        let curr = [0,0]
+        
+        while (len(active_coors) > 0) {
+            curr = active_coors.shift();
+            
+            if (curr[0]-1 >= 0) {
+                if (!inactive_coors.includes([curr[0]-1,curr[1]]) && env[curr[0]-1][curr[1]] == " ") {
+                    inactive_coors[inactive_coors.length] = [curr[0]-1,curr[1]];
+                    active_coors[active_coors.length] = [curr[0]-1,curr[1]];
+                }
+            }
+            // FIX THESE ONES LATER
+
+            // if (curr[0]+1 < len(env)) {
+            //     if ([curr[0]+1,curr[1]] not in inactive_coors and env[curr[0]+1][curr[1]] == " "):
+            //         inactive_coors.append([curr[0]+1,curr[1]])
+            //         active_coors.append([curr[0]+1,curr[1]])
+            // }
+            // if (curr[1]-1 >= 0) {
+            //     if ([curr[0],curr[1]-1] not in inactive_coors and env[curr[0]][curr[1]-1] == " "):
+            //         inactive_coors.append([curr[0],curr[1]-1])
+            //         active_coors.append([curr[0],curr[1]-1])
+            // }
+            // if (curr[1]+1 < len(env[0])) {
+            //     if ([curr[0],curr[1]+1] not in inactive_coors and env[curr[0]][curr[1]+1] == " "):
+            //         inactive_coors.append([curr[0],curr[1]+1])
+            //         active_coors.append([curr[0],curr[1]+1])
+            // }
+        }
+                    
+        return inactive_coors
     }
 }
 
